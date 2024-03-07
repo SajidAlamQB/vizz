81cae2c85b4d972b9a647e399660eb86cc6550fe
Sajid Alam <sajid_alam@mckinsey.com>
2024-03-07 15:28:26 +0000

    Initial commit

---
+++ data/01_raw/sample.sql
@@ -0,0 +1,13 @@
+﻿DROP TABLE IF EXISTS test_table;
+
+CREATE TABLE test_table (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    value REAL NOT NULL
+);
+
+INSERT INTO test_table (name, value) VALUES ('Item 1', 10.5);
+INSERT INTO test_table (name, value) VALUES ('Item 2', 20.75);
+INSERT INTO test_table (name, value) VALUES ('Item 3', 30.00);
+INSERT INTO test_table (name, value) VALUES ('Item 4', 40.25);
+INSERT INTO test_table (name, value) VALUES ('Item 5', 50.50);
